1ec259684c6a666e3ccb32c47b4d266e7bb03ffd
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-09-22 10:27:05 -0400

    l4/3 - state hook examples

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,27 @@
+'use client';
+
+import { useState } from "react";
+
 export default function Contact() {
+    const [ name, setName] = useState<string>('');
+
+    // watch name input and update state var as input value changes
+    const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+    }
+
+    // show confirmation when button clicked
+    const handleSubmit = () => {
+
+    }
+
     return (
         <main>
             <h1>Contact Us</h1>
-            <p>Contact us at 705.000.0000</p>
+            <p>Contact us at 705.000.0000 or send us a message below</p>
+            <input type="text" placeholder="Your Name" value={name} onChange={handleNameChange} />
+            <p>Your name is: {name}</p>
+            <button onClick={handleSubmit}>Submit</button>
         </main>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
-import Image from "next/image";
+'use client';
+
+import { useState } from "react";
 
 export default function Home() {
+  // create a state var to track # of button clicks, defaults to zero
+  const [ count, setCount ] = useState<number>(0);
+
+  const handleClick = () => {
+    // increment the count var by 1 each time 
+    setCount(count + 1);
+  }
+
   // every React component must have 1 return to send the JSX to the DOM
   return (
     <main>
       <h1>COMP2112 Class Site</h1>
       <p>This site is built with Next.js</p>
+      <section>
+        <button onClick={handleClick}>Click Me</button>
+        <p>You have clicked the button {count} times.</p>
+      </section>
     </main>
   );  
 }
