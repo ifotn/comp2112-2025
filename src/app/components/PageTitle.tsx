ce455f4adb7112d424de6f71fb0438283bc06411
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-09-29 10:10:09 -0400

    l5/6 - fix PageTitle props; ensure it returns something

--- src/app/components/PageTitle.tsx
+++ src/app/components/PageTitle.tsx
@@ -2,8 +2,12 @@
 
 import { useEffect } from "react";
 
+type PageTitleProps = {
+    title: string
+}
+
 // component receives a title prop as a string (unique on each page)
-export default function PageTitle({ title }: { title: string}) {
+export default function PageTitle({ title }: PageTitleProps) {
     // trigger this effect and update the page title every time the title prop value changes
     // we need to use Effect Hook as layout only loads once
     useEffect(() => {
@@ -11,5 +15,5 @@ export default function PageTitle({ title }: { title: string}) {
     });
 
     // React components must have a return statement EVEN IF they don't render any JSX to browser
-    //return null;
+    return null;
 }
