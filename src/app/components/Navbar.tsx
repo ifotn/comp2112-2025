be15fd95f549cc65855833dcaa49aa598a3435d0
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-09-29 10:38:13 -0400

    l5/7 - path for Active link in navbar, styles => css

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,14 +1,34 @@
+'use client';
+
 import Link from "next/link";
 
+import { usePathname } from "next/navigation";
+
 export default function Navbar() {
+    // get current url
+    const pathname = usePathname();
+
+    // set link class depending if it matches the current url or not (active / inactive)
+    const getLinkClass = (href:string) => {
+        if (pathname === href || pathname.startsWith(href + '/')) {
+            return 'activeLink';
+        }
+        else {
+            return 'inactiveLink';
+        }
+    }
+
     return (
-        <nav className="bg-gray-900 text-white p-4 flex flex-col md:flex-row md:justify-between md:items-center">
-           <h1 className="text-xl font-bold mb-2 md:mb-0">COMP2112 App</h1>
-           <ul className="flex flex-col md:flex-row md:space-x-4">
-               <li><Link href="/" className="hover:text-gray-300">Home</Link></li>
-               <li><Link href="/about" className="hover:text-gray-300">About</Link></li>
-               <li><Link href="/contact" className="hover:text-gray-300">Contact</Link></li>
-               <li><Link href="/blog" className="hover:text-gray-300">Blog</Link></li>
+        <nav>
+           <h1 className="brand">
+                <Link href="/">
+                    COMP2112 App
+                </Link>
+            </h1>
+           <ul className="navElements">
+               <li><Link href="/about" className={getLinkClass('/about')}>About</Link></li>
+               <li><Link href="/contact" className={getLinkClass('/contact')}>Contact</Link></li>
+               <li><Link href="/blog" className={getLinkClass('/blog')}>Blog</Link></li>
            </ul>
        </nav>
     );
