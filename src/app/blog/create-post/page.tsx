1a3c91c188621a07391a2cadf9743327bb4e14e4
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-10-06 10:10:20 -0400

    l6/5 - api post attempt

--- src/app/blog/create-post/page.tsx
+++ src/app/blog/create-post/page.tsx
@@ -12,8 +12,32 @@ export default function CreatePost() {
     // form input handling.  register: for binding form inputs
     const { register, handleSubmit, formState: { errors, isSubmitSuccessful }} = useForm<PostFormData>();
 
-    const onSubmit = (data: PostFormData) => {
+    const onSubmit = async (data: PostFormData) => {
         console.log(`Submitted: ${data}`);
+
+        try {
+            // get current timestamp
+            const postDate: string = new Date().toLocaleDateString();
+
+            // make POST request to Rich's blog api
+            const res: Response = await fetch('https://vercel-blog-api-eta.vercel.app/api/v1/posts', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    title: data.title,
+                    content: data.content,
+                    username: 'rich.freeman@georgiancollege.ca',
+                    date: postDate
+                })
+            });
+
+            // api response that comes back
+            const apiRes: Response = await res.json();
+            console.log(apiRes);
+        }
+        catch (error: unknown) {
+            console.log(error);
+        }
     }
 
     return (
